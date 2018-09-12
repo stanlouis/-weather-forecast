ec2c143f395c57fd9cdfb50f402a353e9c6c66e0
Stanley Louis <stanlouis@msn.com>
2018-09-12 10:05:19 -0400

    Make SearchBar a controlled component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = {
+    term: ''
+  };
+
+  onInputChange = e => {
+    console.log(e.target.value);
+    this.setState({ term: e.target.value });
+  };
+  
+  onFormSubmit = e => e.preventDefault();
+
   render() {
     return (
-      <form className="input-group">
-        <input type="text" />
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button className="btn btn-secondary" type="submit">
             Submit
